d578307a605e5e4c5518342b166acd3e811f28bd
Diego de Oliveira Alves Pereira <diego_oliveira_alves@hotmail.com>
2016-03-04 20:20:28 -0300

    classes domain v1

--- domain/src/main/java/br/ensalamento/domain/Grade.java
+++ domain/src/main/java/br/ensalamento/domain/Grade.java
@@ -1,5 +1,48 @@
 package br.ensalamento.domain;
 
-public class Grade {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Grade implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_grade", nullable = false)
+	private Long idGrade;
+	
+	//~~ENUM
+	
+	@ManyToOne
+	private Professor professor;
+	
+	@ManyToOne
+	private Materia materia;
+	
+	@ManyToOne
+	private Turma turma;
+	
+	public Long getIdGrade() {
+		return idGrade;
+	}
+	
+	public Professor getProfessor() {
+		return professor;
+	}
+	
+	public Materia getMateria() {
+		return materia;
+	}
+
+	public Turma getTurma() {
+		return turma;
+	}
 }
--- domain/src/main/java/br/ensalamento/domain/Turma.java
+++ domain/src/main/java/br/ensalamento/domain/Turma.java
@@ -1,5 +1,33 @@
 package br.ensalamento.domain;
 
-public class Turma {
+import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Turma implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_turma")
+	private Long idTurma;
+	
+	@OneToMany(mappedBy = "turma", targetEntity = Curso.class)
+	@JoinColumn(nullable = false)
+	private List<Curso> cursos;
+	
+	@OneToMany(mappedBy = "turma", targetEntity = Grade.class)
+	@JoinColumn(nullable = false)
+	private List<Grade> grades;
+	
+	
 }
